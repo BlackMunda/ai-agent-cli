94b70032a929e8653fcde4041362e05d6d2601af
BlackMunda <devashishsingh488@gmail.com>
2026-02-08 18:49:19 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -31,67 +31,112 @@ func main() {
 	}
 
 	client := openai.NewClient(option.WithAPIKey(apiKey), option.WithBaseURL(baseUrl))
-	resp, err := client.Chat.Completions.New(context.Background(),
-		openai.ChatCompletionNewParams{
-			Model: "anthropic/claude-haiku-4.5",
-			Messages: []openai.ChatCompletionMessageParamUnion{
-				{
-					OfUser: &openai.ChatCompletionUserMessageParam{
-						Content: openai.ChatCompletionUserMessageParamContentUnion{
-							OfString: openai.String(prompt),
-						},
-					},
+	messages := []openai.ChatCompletionMessageParamUnion{
+		{
+			OfUser: &openai.ChatCompletionUserMessageParam{
+				Content: openai.ChatCompletionUserMessageParamContentUnion{
+					OfString: openai.String(prompt),
 				},
 			},
-			Tools: []openai.ChatCompletionToolUnionParam{
-				openai.ChatCompletionFunctionTool(openai.FunctionDefinitionParam{
-					Name:        "Read",
-					Description: openai.String("Read and return the content of the file"),
-					Parameters: openai.FunctionParameters{
-						"type": "object",
-						"properties": map[string]any{
-							"file_path": map[string]any{
-								"type":        "string",
-								"description": "the path of the file to read",
-							},
-						},
-						"required": []string{"file_path"},
-					},
-				}),
-			},
 		},
-	)
+	}
+	tools := []openai.ChatCompletionToolUnionParam{
+		openai.ChatCompletionFunctionTool(openai.FunctionDefinitionParam{
+			Name:        "Read",
+			Description: openai.String("Read and return the content of the file"),
+			Parameters: openai.FunctionParameters{
+				"type": "object",
+				"properties": map[string]any{
+					"file_path": map[string]any{
+						"type":        "string",
+						"description": "the path of the file to read",
+					},
+				},
+				"required": []string{"file_path"},
+			},
+		}),
+	}
+
+	result, err := gameLoop(client, messages, tools)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-	if len(resp.Choices) == 0 {
-		panic("No choices in response")
-	}
 
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+	fmt.Fprintln(os.Stdout, result)
+}
 
-	// 1
-	fmt.Print(resp.Choices[0].Message.Content)
+func gameLoop(client openai.Client, messages []openai.ChatCompletionMessageParamUnion, tools []openai.ChatCompletionToolUnionParam) (string, error) {
+	n := 0
 
-	// 2 workaround for test
-	if toolCalls := resp.Choices[0].Message.ToolCalls; len(toolCalls) > 0 {
-		toolCall := toolCalls[0]
-		toolCallFunction := toolCall.Function
-		toolCallFunctionArgs := toolCallFunction.Arguments
-
-		var jsonArgs struct {
-			FilePath string `json:"file_path"`
-		}
-		err = json.Unmarshal([]byte(toolCallFunctionArgs), &jsonArgs)
+	for {
+		resp, err := client.Chat.Completions.New(context.Background(),
+			openai.ChatCompletionNewParams{
+				Model:    "anthropic/claude-haiku-4.5",
+				Messages: messages,
+				Tools:    tools,
+			},
+		)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			return "", err
 		}
 
-		data, err := os.ReadFile(jsonArgs.FilePath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		if len(resp.Choices) == 0 {
+			panic("No choices in response")
 		}
-		fmt.Println(string(data))
+
+		fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+
+		// 1
+		fmt.Print(resp.Choices[0].Message.Content)
+
+		// 2 workaround for test
+		messages = append(messages, openai.ChatCompletionMessageParamUnion{
+			OfAssistant: &openai.ChatCompletionAssistantMessageParam{
+				Content: openai.ChatCompletionAssistantMessageParamContentUnion{
+					OfString: openai.String(resp.Choices[0].Message.Content),
+				},
+			},
+		},
+		)
+
+		var data []byte
+
+		// rangeover toolcalls
+		if toolCalls := resp.Choices[0].Message.ToolCalls; len(toolCalls) > 0 {
+			for i := range toolCalls {
+				toolCall := toolCalls[i]
+				toolCallFunction := toolCall.Function
+				toolCallFunctionArgs := toolCallFunction.Arguments
+
+				// only doing Read here as for now without even looking the name of function
+				var jsonArgs struct {
+					FilePath string `json:"file_path"`
+				}
+				err = json.Unmarshal([]byte(toolCallFunctionArgs), &jsonArgs)
+				if err != nil {
+					return "", err
+				}
+
+				data, err = os.ReadFile(jsonArgs.FilePath)
+				if err != nil {
+					return "", err
+				}
+
+				messages = append(messages,
+					openai.ChatCompletionMessageParamUnion{
+						OfTool: &openai.ChatCompletionToolMessageParam{
+							ToolCallID: toolCall.ID,
+							Content: openai.ChatCompletionToolMessageParamContentUnion{
+								OfString: openai.String(string(data)),
+							},
+						},
+					},
+				)
+			}
+		} else {
+			return string(data), nil
+		}
+		n++
 	}
 }
